f6aabb20e44342ffbcd543cf7d83ae9a27042471
Dark-Mary <133025631+Dark-Mary@users.noreply.github.com>
2025-02-21 09:57:09 -0300

    Create ap.js

---
+++ ap.js
@@ -0,0 +1,33 @@
+
+// Lista de amigos
+let listaDeNomes = [];
+
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome");
+        return;
+    }
+
+    listaDeNomes.push(nome); 
+    document.getElementById('amigo').value = ""; 
+
+
+    let listaElement = document.getElementById('listaAmigos');
+    let item = document.createElement('li'); 
+    item.textContent = nome; 
+    listaElement.appendChild(item); 
+}
+
+
+function sortearAmigo() {
+    if (listaDeNomes.length === 0) {
+        alert("A lista está vazia. Adicione amigos antes de sortear.");
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * listaDeNomes.length);
+    alert(`Amigo sorteado: ${listaDeNomes[indiceSorteado]}`);
+}
